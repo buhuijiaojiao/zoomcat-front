6e44dcf29375521de0955644b70758189e0ad8ce
bhjj <3317038611@qq.com>
2025-07-16 15:36:54 +0800

    🐛fix: 修复git-hooks不正确生效

--- .simple-git-hooks.js
+++ .simple-git-hooks.js
@@ -1,11 +1,12 @@
 export default {
   /**
    * git钩子配置
+   * 注: 每次更改钩子的脚本都需要pnpm simple-git-hooks
    * pre-commit: 提交代码前做一些事
    * pre-push: 推代码前做一些事
    * pre-merge: 合并代码前做一些事
    * pre-rebase: rebase 前做一些事
    * ...
    */
-  'pre-commit': 'lint-staged'
+  'pre-commit': 'pnpm lint-staged'
 }
