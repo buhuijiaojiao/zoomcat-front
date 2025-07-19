f69ae15950944d79202bb0712a8740b284ce65c7
bhjj <3317038611@qq.com>
2025-07-19 16:29:42 +0800

    ✨feat: 引入Headless UI组件库

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script lang="ts" setup>
+<script setup>
+import TestHeadlessUI from './TestHeadlessUI.vue'
+
 //
 </script>
 
 <template>
-  <button class="bg-gradient-linear bg-gradient-[135deg,#87ceeb,#4682b4]">
-    演示
-  </button>
+  <div>
+    <TestHeadlessUI />
+  </div>
 </template>
---
+++ src/views/TestHeadlessUI.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center">
+    <button
+      type="button"
+      @click="openModal"
+      class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+    >
+      test
+    </button>
+  </div>
+  <TransitionRoot
+    appear
+    :show="isOpen"
+    as="template"
+  >
+    <Dialog
+      as="div"
+      @close="closeModal"
+      class="relative z-10"
+    >
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex items-center justify-center min-h-full p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="font-medium text-lg leading-6 text-gray-900"
+              >
+                test headlessUI
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">测试成功</p>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="justify-center rounded-md px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 inline-flex border border-transparent bg-blue-100 text-blue-900 hover:bg-blue-200 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  好的
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle
+} from '@headlessui/vue'
+
+const isOpen = ref(true)
+
+function closeModal() {
+  isOpen.value = false
+}
+function openModal() {
+  isOpen.value = true
+}
+</script>
